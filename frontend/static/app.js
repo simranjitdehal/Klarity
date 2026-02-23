7ddc34ab3306d3fe9c8396623010aa640b546ee1
Simranjit Singh Dehal <simranjitsinghdehal@gmail.com>
2026-02-23 14:14:46 +0530

    m

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -3,7 +3,7 @@ async function sendLogs() {
 
     const parsedInput = JSON.parse(rawInput); // convert textarea text into object
 
-    const response = await fetch("klarity-production-6a25.up.railway.app/ingest", {
+    const response = await fetch("https://klarity-production-6a25.up.railway.app/ingest", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
