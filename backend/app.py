959ededa46e8e04243b3489f856f6e9919fdef2e
Simranjit Singh Dehal <simranjitsinghdehal@gmail.com>
2026-03-01 23:15:53 +0530

    added temp  automation

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://klarityai.up.railway.app", "http://localhost:3000"],  # restrict later
+    allow_origins=["https://klarityai.up.railway.app", "http://127.0.0.1:8000", "http://localhost:3000"],  # restrict later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,17 +1,37 @@
-async function sendLogs() {
-    const rawInput = document.getElementById("logInput").value;
+// async function sendLogs() {
+//     const rawInput = document.getElementById("logInput").value;
 
-    const parsedInput = JSON.parse(rawInput); // convert textarea text into object
+//     const parsedInput = JSON.parse(rawInput); // convert textarea text into object
 
-    const response = await fetch("https://klarity-production-6a25.up.railway.app/ingest", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(parsedInput) // send it directly
-    });
+//     // const response = await fetch("https://klarity-production-6a25.up.railway.app/ingest", { # for production
+//     const response = await fetch("http://127.0.0.1:8000/ingest", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(parsedInput) // send it directly
+//     });
+
+//     const data = await response.json();
+//     console.log(data);
+//     document.getElementById("output").innerText = data.analysis.summary;
+// }
+
+async function fetchLatest() {
+    const response = await fetch(
+        "https://klarity-production-6a25.up.railway.app/latest"
+    );
 
     const data = await response.json();
-    console.log(data);
-    document.getElementById("output").innerText = data.analysis.summary;
+
+    if (data.analysis) {
+        document.getElementById("output").innerText =
+            data.analysis.summary;
+    }
 }
+
+// Fetch immediately on page load
+fetchLatest();
+
+// Then poll every 5 seconds
+setInterval(fetchLatest, 5000);
